deb5df48f4220a3e37745d4a7c531082a2961891
Sharykhin <siarhei.sharykhin@gmail.com>
2018-03-24 23:17:06 +0300

    left a few todos

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -62,12 +62,14 @@ func (p *Proxy) parseRequest(r *http.Request) *httputil.ReverseProxy {
 // Handle handles all income requests.
 // It fills it up with some additional headers and pass to a target server
 func (p *Proxy) Handle(w http.ResponseWriter, r *http.Request) {
+	// TODO: Move CORS to a separate middleware
 	if r.Method == "OPTIONS" {
 		return
 	}
 	w.Header().Set("Access-Control-Allow-Origin", corsOrigin)
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
+	// TODO: Move Additional headers to a separate middleware
 	w.Header().Set("X-GoProxy", "GoProxy")
 	w.Header().Set("X-Forwarded-Proto", "http")
 	w.Header().Set("X-Real-IP", r.RemoteAddr)
